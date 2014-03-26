dbbbf84e43819fded88e6826f3a7238e49210544
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-03-26 17:23:57 +0100

    Adding more case statements to handle schema validation

--- spec/JsonSchema/Schema/AbstractSchemaSpec.php
+++ spec/JsonSchema/Schema/AbstractSchemaSpec.php
@@ -2,10 +2,13 @@
 
 namespace spec\JsonSchema\Schema;
 
+use JsonSchema\Enum\StrictnessMode;
 use JsonSchema\Schema\AbstractSchema;
+use JsonSchema\Validator\Constraint\ArrayConstraint;
 use JsonSchema\Validator\Constraint\BooleanConstraint;
 use JsonSchema\Validator\Constraint\ConstraintInterface;
 use JsonSchema\Validator\Constraint\NumberConstraint;
+use JsonSchema\Validator\Constraint\ObjectConstraint;
 use JsonSchema\Validator\Constraint\StringConstraint;
 use JsonSchema\Validator\SchemaValidator;
 use JsonSchema\Validator\ValidatorInterface;
@@ -20,16 +23,8 @@ class AbstractSchemaSpec extends ObjectBehavior
 
     function let(SchemaValidator $validator, StringConstraint $constraint)
     {
-        $data = (object) ['title' => 'foo'];
-
-        // Define collaborator promises
-        $validator->createConstraint('StringConstraint', 'foo')->willReturn($constraint);
-        $validator->addConstraint($constraint)->shouldBeCalled();
-        $validator->validate()->shouldBeCalled();
-
-        // Basic object setup
         $this->beAnInstanceOf('spec\JsonSchema\Schema\TestableAbstractSchema');
-        $this->beConstructedWith($validator, $data);
+        $this->beConstructedWith($validator, (object) ['foo' => 'bar']);
     }
 
     function it_should_throw_exception_if_schema_data_is_not_object()
@@ -80,6 +75,7 @@ class AbstractSchemaSpec extends ObjectBehavior
         $name = 'title';
 
         $this->setupKeywordCollaborators($val, $validator, $constraint);
+        $validator->validate()->shouldBeCalled();
 
         // Fake a "FALSE" validation
         $this->offsetSet($name, $val);
@@ -92,17 +88,224 @@ class AbstractSchemaSpec extends ObjectBehavior
 
     function it_should_validate_title_as_string(SchemaValidator $validator, StringConstraint $constraint)
     {
-        $this->testValidationPrediction('title', 'StringConstraint', $validator, $constraint);
+        $this->testValidationPrediction('title', $validator, $constraint);
     }
 
     function it_should_validate_desc_as_string(SchemaValidator $validator, StringConstraint $constraint)
     {
-        $this->testValidationPrediction('description', 'StringConstraint', $validator, $constraint);
+        $this->testValidationPrediction('description', $validator, $constraint);
+    }
+
+    function it_should_validate_multipleOf_as_number_greater_than_0(SchemaValidator $validator, NumberConstraint $constraint)
+    {
+        $this->testValidationPrediction('multipleOf', $validator, $constraint);
+    }
+
+    function it_should_validate_maximum_as_number(SchemaValidator $validator, NumberConstraint $constraint)
+    {
+        $this->testValidationPrediction('maximum', $validator, $constraint);
+    }
+
+    function it_should_validate_exclusiveMaximum_as_boolean(SchemaValidator $validator, BooleanConstraint $constraint)
+    {
+        $this->testValidationPrediction('exclusiveMaximum', $validator, $constraint);
+    }
+
+    function it_should_validate_minimum_as_number(SchemaValidator $validator, NumberConstraint $constraint)
+    {
+        $this->testValidationPrediction('minimum', $validator, $constraint);
+    }
+
+    function it_should_validate_exclusiveMinimum_as_boolean(SchemaValidator $validator, BooleanConstraint $constraint)
+    {
+        $this->testValidationPrediction('exclusiveMinimum', $validator, $constraint);
+    }
+
+    function it_should_validate_maxLength_as_number(SchemaValidator $validator, NumberConstraint $constraint)
+    {
+        $constraint->setLowerBound(Argument::type('int'))->shouldBeCalled();
+        $constraint->setExclusive(Argument::type('bool'))->shouldBeCalled();
+
+        $this->testValidationPrediction('maxLength', $validator, $constraint);
+    }
+
+    function it_should_validate_minLength_as_number(SchemaValidator $validator, NumberConstraint $constraint)
+    {
+        $constraint->setLowerBound(Argument::type('int'))->shouldBeCalled();
+        $constraint->setExclusive(Argument::type('bool'))->shouldBeCalled();
+
+        $this->testValidationPrediction('minLength', $validator, $constraint);
+    }
+
+    function it_should_validate_pattern_as_regex_string(SchemaValidator $validator, StringConstraint $constraint)
+    {
+        $constraint->setRegexValidation(Argument::type('bool'))->shouldBeCalled();
+
+        $this->testValidationPrediction('pattern', $validator, $constraint);
+    }
+
+    function it_should_validate_additionalItems_as_boolean(
+        SchemaValidator $validator,
+        BooleanConstraint $booleanConstraint,
+        ObjectConstraint $objectConstraint
+    ) {
+        // Make sure the validator knows either constraint is acceptable
+        $validator->setStrictnessMode(StrictnessMode::ANY)->shouldBeCalled();
+
+        // Objects need to be valid schemas
+        $objectConstraint->setSchemaValidation(Argument::type('bool'))->shouldBeCalled();
+
+        $value = 'foo';
+        $name = 'additionalItems';
+
+        // Make sure the method is stubbed
+        $validator->createConstraint('BooleanConstraint', Argument::any())->willReturn($booleanConstraint);
+        $validator->createConstraint('ObjectConstraint', Argument::any())->willReturn($objectConstraint);
+
+        // Now initiate validator name search
+        $this->getKeywordConstraints($name, $value);
+
+        // Make sure our expectation has been fulfilled
+        $validator->createConstraint('BooleanConstraint', $value)->shouldHaveBeenCalled();
+        $validator->createConstraint('ObjectConstraint', $value)->shouldHaveBeenCalled();
+    }
+
+    function it_should_validate_items_as_either_object_or_array(
+        SchemaValidator $validator,
+        ObjectConstraint $objectConstraint,
+        ArrayConstraint $arrayConstraint
+    ) {
+        // Make sure the validator knows either constraint is acceptable
+        $validator->setStrictnessMode(StrictnessMode::ANY)->shouldBeCalled();
+
+        // Objects need to be valid schemas
+        $objectConstraint->setSchemaValidation(Argument::type('bool'))->shouldBeCalled();
+
+        // Array items need to be valid schemas
+        $arrayConstraint->setNestedSchemaValidation(Argument::type('bool'))->shouldBeCalled();
+
+        $value = 'foo';
+        $name = 'items';
+
+        // Make sure the method is stubbed
+        $validator->createConstraint('ArrayConstraint', Argument::any())->willReturn($arrayConstraint);
+        $validator->createConstraint('ObjectConstraint', Argument::any())->willReturn($objectConstraint);
+
+        // Now initiate validator name search
+        $this->getKeywordConstraints($name, $value);
+
+        // Make sure our expectation has been fulfilled
+        $validator->createConstraint('ArrayConstraint', $value)->shouldHaveBeenCalled();
+        $validator->createConstraint('ObjectConstraint', $value)->shouldHaveBeenCalled();
+    }
+
+    function it_should_validate_maxItems_as_number_greater_than_or_equal_to_0(
+        SchemaValidator $validator, NumberConstraint $constraint
+    ) {
+        $constraint->setLowerBound(0)->shouldBeCalled();
+        $constraint->setExclusive(false)->shouldBeCalled();
+
+        $this->testValidationPrediction('maxItems', $validator, $constraint);
+    }
+
+    function it_should_validate_minItems_as_number_greater_than_or_equal_to_0(
+        SchemaValidator $validator, NumberConstraint $constraint
+    ) {
+        $constraint->setLowerBound(0)->shouldBeCalled();
+        $constraint->setExclusive(false)->shouldBeCalled();
+
+        $this->testValidationPrediction('minItems', $validator, $constraint);
+    }
+
+    function it_should_validate_uniqueItems_as_bool(SchemaValidator $validator, BooleanConstraint $constraint)
+    {
+        $this->testValidationPrediction('uniqueItems', $validator, $constraint);
+    }
+
+    function it_should_validate_maxProperties_as_number_greater_than_or_equal_to_0(
+        SchemaValidator $validator, NumberConstraint $constraint
+    ) {
+        $constraint->setLowerBound(0)->shouldBeCalled();
+        $constraint->setExclusive(false)->shouldBeCalled();
+
+        $this->testValidationPrediction('maxProperties', $validator, $constraint);
+    }
+
+    function it_should_validate_minProperties_as_number_greater_than_or_equal_to_0(
+        SchemaValidator $validator, NumberConstraint $constraint
+    ) {
+        $constraint->setLowerBound(0)->shouldBeCalled();
+        $constraint->setExclusive(false)->shouldBeCalled();
+
+        $this->testValidationPrediction('minProperties', $validator, $constraint);
+    }
+
+    function it_should_validate_required_as_array(SchemaValidator $validator, ArrayConstraint $constraint)
+    {
+        $constraint->setInternalType('string')->shouldBeCalled();
+        $constraint->setUniqueness(true)->shouldBeCalled();
+        $constraint->setMinimumCount(1)->shouldBeCalled();
+        $this->testValidationPrediction('required', $validator, $constraint);
+    }
+
+    function it_should_validate_additionalProperties_as_either_bool_or_object()
+    {
+        
+    }
+
+    function it_should_consider_additionalProperties_a_json_schema_if_object()
+    {
+
+    }
+
+    function it_should_validate_properties_as_object()
+    {
+
+    }
+
+    function it_should_consider_properties_an_object_whose_properties_are_json_schemas()
+    {
+
+    }
+
+    function it_should_validate_patternProperties_as_object()
+    {
+
+    }
+
+    function it_should_insist_on_patternProperties_having_regex_strings_for_object_properties()
+    {
+
+    }
+
+    function it_should_insist_on_patternProperties_having_json_schemas_for_object_vals()
+    {
+
+    }
+
+    function it_should_validate_dependencies_as_object()
+    {
+
+    }
+
+    function it_should_insist_on_dependencies_having_either_arrays_or_objects_as_object_properties()
+    {
+
+    }
+
+    function it_should_ensure_that_values_of_dependencies_which_are_values_are_json_schemas()
+    {
+
+    }
+
+    function it_should_ensure_that_values_of_dependencies_which_are_arrays_contain_unique_strings()
+    {
+
     }
 
     /*** HELPERS ***/
 
-    private function testValidationPrediction($name, $constraintName, ValidatorInterface $validator, ConstraintInterface $constraint)
+    private function testValidationPrediction($name, ValidatorInterface $validator, ConstraintInterface $constraint)
     {
         // Value is arbitrary
         $value = 'foo';
@@ -110,38 +313,17 @@ class AbstractSchemaSpec extends ObjectBehavior
         // Make sure the method is stubbed
         $validator->createConstraint(Argument::any(), Argument::any())->willReturn($constraint);
 
-        // Set up explicit expectation
-        $validator->createConstraint($constraintName, $value)->shouldBeCalled();
-
         // Now initiate validator name search
         $this->getKeywordConstraints($name, $value);
+
+        // Make sure our expectation has been fulfilled
+        $validator->createConstraint($this->getCollaboratorName($constraint), $value)->shouldHaveBeenCalled();
     }
 
     public function testMutability($key, $val)
     {
         $this->offsetSet($key, $val);
         $this->offsetGet($key)->shouldReturn($val);
-    }
-
-    public static function getWrongDataTypes($correctType)
-    {
-        $allTypes = [
-            'string'   => 'foo',
-            'int'      => 1,
-            'float'    => 2.5,
-            'bool'     => true,
-            'object'   => new \stdClass(),
-            'array'    => [],
-            'resource' => fopen('php://temp', 'r+')
-        ];
-
-        if ($correctType == 'numeric') {
-            $correctType = ['int', 'float'];
-        }
-
-        $correctTypes = array_flip((array) $correctType);
-
-        return array_diff_key($allTypes, $correctTypes);
     }
 
     private function getCollaboratorName(Collaborator $object)
--- src/JsonSchema/Schema/AbstractSchema.php
+++ src/JsonSchema/Schema/AbstractSchema.php
@@ -4,6 +4,7 @@ namespace JsonSchema\Schema;
 
 use JsonSchema\ArrayAccessTrait;
 use JsonSchema\Enum\SchemaKeyword;
+use JsonSchema\Enum\StrictnessMode;
 use JsonSchema\Validator\Constraint\ConstraintInterface;
 use JsonSchema\Validator\ValidatorInterface;
 
@@ -64,20 +65,82 @@ abstract class AbstractSchema implements SchemaInterface
         $constraints = [];
 
         switch ($keyword) {
+            // String
             case SchemaKeyword::TITLE:
             case SchemaKeyword::DESCRIPTION:
                 $constraints[] = $this->validator->createConstraint('StringConstraint', $value);
                 break;
+
+            // Regex string
+            case SchemaKeyword::PATTERN:
+                $constraint = $this->validator->createConstraint('StringConstraint', $value);
+                $constraint->setRegexValidation(true);
+                $constraints[] = $constraint;
+                break;
+
+            // Numeric
+            case SchemaKeyword::MINIMUM:
+            case SchemaKeyword::MAXIMUM:
+                $constraints[] = $this->validator->createConstraint('NumberConstraint', $value);
+                break;
+
+            // Integer > 0
             case SchemaKeyword::MULTIPLE_OF:
                 $constraint = $this->validator->createConstraint('NumberConstraint', $value);
                 $constraint->setLowerBound(0);
                 $constraints[] = $constraint;
                 break;
-            case SchemaKeyword::MAXIMUM:
-                $constraints[] = $this->validator->createConstraint('NumberConstraint', $value);
-                break;
+
+            // Integer >= 0
+            case SchemaKeyword::MAX_LENGTH:
+            case SchemaKeyword::MIN_LENGTH:
+            case SchemaKeyword::MAX_ITEMS:
+            case SchemaKeyword::MIN_ITEMS:
+            case SchemaKeyword::MAX_PROPERTIES:
+            case SchemaKeyword::MIN_PROPERTIES:
+                $constraint = $this->validator->createConstraint('NumberConstraint', $value);
+                $constraint->setLowerBound(0);
+                $constraint->setExclusive(false);
+                $constraints[] = $constraint;
+
+            // Boolean
+            case SchemaKeyword::EXCLUSIVE_MINIMUM:
             case SchemaKeyword::EXCLUSIVE_MAXIMUM:
+            case SchemaKeyword::UNIQUE_ITEMS:
                 $constraints[] = $this->validator->createConstraint('BooleanConstraint', $value);
+                break;
+
+            // Bool or object
+            case SchemaKeyword::ADDITIONAL_ITEMS:
+                // Make sure the validator knows either constraint can succeed for validation to pass
+                $this->validator->setStrictnessMode(StrictnessMode::ANY);
+                // Add bool constraint
+                $constraints[] = $this->validator->createConstraint('BooleanConstraint', $value);
+                // Add object constraint + ensure schema validation
+                $objectConstraint = $this->validator->createConstraint('ObjectConstraint', $value);
+                $objectConstraint->setSchemaValidation(true);
+                $constraints[] = $objectConstraint;
+                break;
+
+            // Object or array
+            case SchemaKeyword::ITEMS:
+                $this->validator->setStrictnessMode(StrictnessMode::ANY);
+                // Add array constraint + ensure nested schema validation
+                $constraint = $this->validator->createConstraint('ArrayConstraint', $value);
+                $constraint->setNestedSchemaValidation(true);
+                $constraints[] = $constraint;
+                // Add object constraint + ensure schema validation
+                $objectConstraint = $this->validator->createConstraint('ObjectConstraint', $value);
+                $objectConstraint->setSchemaValidation(true);
+                $constraints[] = $objectConstraint;
+                break;
+
+            case SchemaKeyword::REQUIRED:
+                $constraint = $this->validator->createConstraint('ArrayConstraint', $value);
+                $constraint->setInternalType('string');
+                $constraint->setUniqueness(true);
+                $constraint->setMinimumCount(1);
+                $constraints[] = $constraint;
                 break;
         }
 
